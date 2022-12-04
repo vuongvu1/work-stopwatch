d76453ba9f97738d38aef25e6866e91fac6b06b4
Vuong Vu <hoangvuong.v99@gmail.com>
2022-12-04 23:05:13 +0100

    Simplify basic function

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,39 @@ const to2numString = (value: number): string => {
   return `${value}`;
 };
 
+function openFullscreen() {
+  const element = document.querySelector("body");
+  if (!element) return;
+
+  element
+    .requestFullscreen()
+    .then(function () {
+      // element has entered fullscreen mode successfully
+    })
+    .catch(function (error) {
+      // element could not enter fullscreen mode
+      // error message
+      console.log(error.message);
+    });
+}
+
+function exitFullscreen() {
+  document
+    .exitFullscreen()
+    .then(function () {
+      // element has exited fullscreen mode
+    })
+    .catch(function (error) {
+      // element could not exit fullscreen mode
+      // error message
+      console.log(error.message);
+    });
+}
+
 function App() {
   const [isPlay, setPlay] = useState(false);
   const [workTimer, setWorkTimer] = useState(0);
+  const [isFullscreen, setFullscreen] = useState(false);
 
   useEffect(() => {
     let playRef: ReturnType<typeof setInterval> = 0;
@@ -34,13 +64,26 @@ function App() {
       <div className="stopwatch">
         {to2numString(hour)}:{to2numString(minute)}:{to2numString(second)}
       </div>
-      <button className="play-btn" onClick={() => setPlay(true)}>
-        ▶️ Work
+      <button className="play-btn" onClick={() => setPlay((p) => !p)}>
+        {isPlay ? "⏸️ Pause" : "▶️ Work"}
       </button>
       {/* <button className="play-btn">▶️ Learn</button>
       <button className="play-btn">▶️ Play</button> */}
-      <button className="play-btn" onClick={() => setPlay(false)}>
-        ⏸️ Pause
+      {/* <button className="play-btn" onClick={() => setPlay(false)}>
+        ⏸️ Pause All
+      </button> */}
+      <button
+        className="play-btn"
+        onClick={() => {
+          if (isFullscreen) {
+            exitFullscreen();
+          } else {
+            openFullscreen();
+          }
+          setFullscreen((f) => !f);
+        }}
+      >
+        {isFullscreen ? "Exit Fullscreen" : "Enter Fullscreen"}
       </button>
     </div>
   );
